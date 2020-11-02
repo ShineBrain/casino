21751e4df3df32b3285afb2a29a163ff8e313dee
PPPPPPika <edik_sidorov@list.ru>
2020-11-02 03:31:22 +0300

    +

--- src/test/Registration/PageRegistration.java
+++ src/test/Registration/PageRegistration.java
@@ -1,4 +1,4 @@
-package test.Registration;
+package Test.Registration;
 
 import BackEnd.Account;
 
@@ -15,12 +15,12 @@ public class PageRegistration {
         this.cardNumber = cardNumber;
     }
 
-    /*public boolean CheckAccount(){
+    public boolean CheckAccount(){
         account.TestCreateAccount();
 
         if (account.CheckDuplicateAccount(login, cardNumber)){
             return false;
         }
         return true;
-    }*/
+    }
 }
